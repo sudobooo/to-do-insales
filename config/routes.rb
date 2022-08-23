c4768913df43e49a6c90c45ce1b57ef06446557b
sudobooo <sergey.masyutin@insales.ru>
2022-08-23 14:00:01 +0300

    Add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'users/new'
+  root 'static_pages#home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  get  'users/new'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,23 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get ''
     assert_response :success
-    assert_select "title", "Home | To Do inSales"
+    assert_select 'title', 'Home | To Do inSales'
   end
 
-  test "should get help" do
+  test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | To Do inSales"
+    assert_select 'title', 'Help | To Do inSales'
   end
 
-  test "should get about" do
+  test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | To Do inSales"
+    assert_select 'title', 'About | To Do inSales'
   end
 end
