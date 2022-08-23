58e8f08681dce64bc30a48920a1ba7dd8c4ea0e7
sudobooo <sergey.masyutin@insales.ru>
2022-08-23 14:39:03 +0300

    Update tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,21 +3,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "| To Do inSales"
+  end
+
   test 'should get home' do
     get ''
     assert_response :success
-    assert_select 'title', 'Home | To Do inSales'
+    assert_select 'title', "Home #{@base_title}"
   end
 
   test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', 'Help | To Do inSales'
+    assert_select 'title', "Help #{@base_title}"
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'About | To Do inSales'
+    assert_select 'title', "About #{@base_title}"
   end
 end
