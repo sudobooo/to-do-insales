cc3fdad0e90b332a7bcd19f8711b35f2791d7ac3
sudobooo <sergey.masyutin@insales.ru>
2022-08-23 17:34:36 +0300

    Add helper for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "To Do inSales"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
