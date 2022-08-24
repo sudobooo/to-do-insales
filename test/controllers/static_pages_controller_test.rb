6f148e53b9a079d4ece129ca9f6b9e27ad615a1b
Serge <92683717+sudobooo@users.noreply.github.com>
2022-08-24 15:16:01 +0300

    Revert "Refactor static pages"
    This reverts commit eb0706d5845a188c644df783cee0274ad3c2fbfe.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,15 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | To Do inSales"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | To Do inSales"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | To Do inSales"
   end
 end
