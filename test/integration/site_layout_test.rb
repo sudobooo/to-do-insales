b93f44fb5df5975e2434c2cd64e2ac53b844cca7
sudobooo <marsdoghelena@gmail.com>
2022-09-13 15:25:54 +0300

    Fix linter errors

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,6 +12,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select 'title', full_title('Sign up')
   end
 end
