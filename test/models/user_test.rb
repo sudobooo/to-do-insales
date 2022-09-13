96cb6f2ffc496abd71e5050ba8079f4bf4a2815d
sudobooo <marsdoghelena@gmail.com>
2022-09-13 18:05:18 +0300

    Fix linter errors

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,21 +3,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
-    @user = User.new(name: "Example User", insales_user_id: 1234)
+    @user = User.new(name: 'Example User', insales_user_id: 1234)
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.name = "     "
+  test 'name should be present' do
+    @user.name = '     '
     assert_not @user.valid?
   end
 
-  test "insales_user_id should be present" do
+  test 'insales_user_id should be present' do
     @user.insales_user_id = nil
     assert_not @user.valid?
   end
