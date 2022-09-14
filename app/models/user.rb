0636e6316d8c04fb6c9f08f99db325d3d214584f
sudobooo <marsdoghelena@gmail.com>
2022-09-14 17:07:30 +0300

    Add the validated standard format for the password column

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :insales_user_id, presence: true, uniqueness: true
   has_secure_password
+  VALID_PASSWORD_REGEX = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/
+  validates :password, presence: true, length: { minimum: 8 },
+                       format: { with: VALID_PASSWORD_REGEX }
 end
