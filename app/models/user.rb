62d305321487614ec58d06c58ad22d0cc1cb2d1d
sudobooo <marsdoghelena@gmail.com>
2022-09-14 14:53:59 +0300

    Add validates for length and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :insales_user_id, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :insales_user_id, presence: true, uniqueness: true
 end
